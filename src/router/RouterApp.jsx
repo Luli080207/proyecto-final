718d80ad996d1c9f964ffcbd484b7f88ef3c5640
Luli080207 <skketchead@gmail.com>
2025-08-07 19:01:13 -0300

    import de la pagina about us

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -5,6 +5,7 @@ import { Login } from "../pages/Login"
 import { Register } from "../pages/Register"
 import { NotFound } from "../pages/NotFound"
 import { PrivateRoute } from "../components/PrivateRoute"
+import About from "../pages/about"
 
 const RouterApp = () => {
   return (
