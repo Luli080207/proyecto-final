d379c150a2138d56cf5a1c0d6f2e890da4a5c75a
Luli080207 <skketchead@gmail.com>
2025-08-07 17:14:22 -0300

    finalizacion de la seccion main

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,10 +2,24 @@ import React from "react";
 
 const About = () => {
   return (
-    <main className="about-page"></main>
+    <main className="about-page">
     <section>
       <h2>Descripción general del proyecto</h2>
       <p>Tienda online en donde se pueden visualizar productos, agregarlos al carrito y si sos administrador, podes editarlos.</p>
     </section>
-    
-}
+
+    <section>
+      <h2>A quién esta dirigido</h2>
+      <p>Está dirigido a cualquier persona que este interesada en aprender sobre el desarrollo web.</p>
+    </section>
+
+    <section>
+      <h2>Tectonologías usadas</h2>
+      <p>Se utilizó react para la parte de frontend, Node.js y Express para el backend.
+      </p>
+    </section>
+    </main>
+  );
+};
+
+export default About;
