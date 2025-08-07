4afea9a5d31512833c61157e8c0ac4b4552ade54
Luli080207 <skketchead@gmail.com>
2025-08-07 17:10:20 -0300

    creacion del apartado about, titulo y texto

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,1 +1,11 @@
 import React from "react";
+
+const About = () => {
+  return (
+    <main className="about-page"></main>
+    <section>
+      <h2>Descripción general del proyecto</h2>
+      <p>Tienda online en donde se pueden visualizar productos, agregarlos al carrito y si sos administrador, podes editarlos.</p>
+    </section>
+    
+}
