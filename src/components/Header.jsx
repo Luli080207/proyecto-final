5b1c370edb5b4e3d5469b0cf29d8f58078cb7a1c
Luli080207 <skketchead@gmail.com>
2025-08-07 18:59:37 -0300

    correccion en los errores de codigo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,31 +9,36 @@ const Header = () => {
   }
 
   return (
-    <header style={{ backgroundColor: "lightblue", padding "10px" }}>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/8/85/Logo-Test.png" 
-      alt="imagen de logo" 
-      style={{height: "50px", marginRight: "20px"}}
-      />
+  <header style={{ backgroundColor: "lightblue", padding: "10px" }}>
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/8/85/Logo-Test.png"
+      alt="imagen de logo"
+      style={{ height: "50px", marginRight: "20px" }}
+    />
 
+    <nav>
+      <ul style={{ display: "flex", gap: "15px", listStyle: "none" }}>
+        <li><Link to="/">Inicio</Link></li>
+        <li><Link to="/about">Sobre Nosotros</Link></li>
 
-      <nav>
-        <ul style={{ display: "flex", gap: "15px", listStyle"none"}}>
-              <li><Link to="/">Inicio</Link></li>
-              {user ? (
-                <>
-              <li><Link to="/dashboard">Dashboard</Link></li>
+        {user ? (
+          <>
+            <li><Link to="/dashboard">Dashboard</Link></li>
+            <li>
               <button onClick={handleLogout}>Cerrar sesión</button>
-            </>
-             ) : (
-             <>
-              <li><Link to="/login">Login</Link></li>
-              <li><Link to="/registrate">Registrate</Link></li>
-            </>
-          )}
-        </ul>
-      </nav>
-    </header>
-  );
+            </li>
+          </>
+        ) : (
+          <>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/registrate">Registrate</Link></li>
+          </>
+        )}
+      </ul>
+    </nav>
+  </header>
+);
+
 };
 
 export { Header }
